4e5dfb6ad9d72dd9cb146c463babf69683ec7c99
Gerardo Martinez <gerardosmartinezg@gmail.com>
2023-10-05 17:49:48 -0400

    Agregar archivos de proyecto.

---
+++ TiendaOnline_API/Models/Articulo.cs
@@ -0,0 +1,14 @@
+﻿namespace TiendaOnline_API.Models
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+
+        public int Codigo { get; set; }
+
+        public string Nombre { get; set;}
+        public string Marca { get; set;}
+        public string Categoria { get; set;}
+        public decimal Precio { get; set;}
+    }
+}
